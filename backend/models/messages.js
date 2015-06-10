fa8c5ca2b186632c329bd9c9d3a0505e0a1dd163
Bart van 't Ende <bartvtende@gmail.com>
2015-06-10 13:27:11 +0200

    SQL dump for MySQL and timestamps for mongo records

--- backend/models/messages.js
+++ backend/models/messages.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
+var timestamps = require('mongoose-timestamp');
 var Schema = mongoose.Schema;
 
 var messagesSchema = new Schema({
     // Empty schema for now (will add later)
 }, { versionKey: false, strict: false });
+
+messagesSchema.plugin(timestamps);
 
 var Messages = mongoose.model('Messages', messagesSchema);
 
