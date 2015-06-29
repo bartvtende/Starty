72aa768c8ff9f18675c5512abea8935826ff81f5
Jerke Combee <jerke1988@gmail.com>
2015-06-29 11:53:51 +0200

    Google Chart Implemented

--- frontend/app/scripts/controllers/project/reports.js
+++ frontend/app/scripts/controllers/project/reports.js
@@ -10,6 +10,124 @@
 angular.module('startyApp')
     .controller('ReportsCtrl', function ($scope) {
 
+    	$scope.chart = {
+  "type": "ColumnChart",
+  "cssStyle": "height:200px; width:300px;",
+  "data": {
+    "cols": [
+      {
+        "id": "month",
+        "label": "Month",
+        "type": "string",
+        "p": {}
+      },
+      {
+        "id": "laptop-id",
+        "label": "Laptop",
+        "type": "number",
+        "p": {}
+      },
+      {
+        "id": "desktop-id",
+        "label": "Desktop",
+        "type": "number",
+        "p": {}
+      },
+      {
+        "id": "server-id",
+        "label": "Server",
+        "type": "number",
+        "p": {}
+      },
+      {
+        "id": "cost-id",
+        "label": "Shipping",
+        "type": "number"
+      }
+    ],
+    "rows": [
+      {
+        "c": [
+          {
+            "v": "January"
+          },
+          {
+            "v": 19,
+            "f": "42 items"
+          },
+          {
+            "v": 12,
+            "f": "Ony 12 items"
+          },
+          {
+            "v": 7,
+            "f": "7 servers"
+          },
+          {
+            "v": 4
+          }
+        ]
+      },
+      {
+        "c": [
+          {
+            "v": "February"
+          },
+          {
+            "v": 13
+          },
+          {
+            "v": 1,
+            "f": "1 unit (Out of stock this month)"
+          },
+          {
+            "v": 12
+          },
+          {
+            "v": 2
+          }
+        ]
+      },
+      {
+        "c": [
+          {
+            "v": "March"
+          },
+          {
+            "v": 24
+          },
+          {
+            "v": 0
+          },
+          {
+            "v": 11
+          },
+          {
+            "v": 6
+          }
+        ]
+      }
+    ]
+  },
+  "options": {
+    "title": "Sales per month",
+    "isStacked": "true",
+    "fill": 20,
+    "displayExactValues": true,
+    "vAxis": {
+      "title": "Sales unit",
+      "gridlines": {
+        "count": 6
+      }
+    },
+    "hAxis": {
+      "title": "Date"
+    }
+  },
+  "formatters": {},
+  "displayed": true
+};
+
         $scope.refreshGraph = function () {
             console.log('Refresh graph initiated!');
         };
